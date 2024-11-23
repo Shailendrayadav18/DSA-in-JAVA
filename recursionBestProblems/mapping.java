fa45153b8597243c895da5537fc9162c999e3433
Shailendrayadav18 <monuyadav1872002@gmail.com>
2024-11-23 22:30:38 +0530

    Solve a mapping question using recursion

--- recursionBestProblems/mapping.java
+++ recursionBestProblems/mapping.java
@@ -4,16 +4,17 @@ public class mapping {
     public static String[] keypad = {".", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tu", "vwx", "yz"};
     public static void printMappedValue(String str, int index, String combination){
         if(index==str.length()){
-
+            System.out.println(combination);
+            return;
         }
         int value = str.charAt(index);
-        for(int i=0; i<keypad[value].length(); i++){
-            char currChar = keypad[value].charAt(i);
+        for(int i=0; i<keypad[value-'0'].length(); i++){
+            char currChar = keypad[value-'0'].charAt(i);
             printMappedValue(str, index+1, combination+currChar);
-            
         }
     }
     public static void main(String[] args) {
-        
+        String str = "23";
+        printMappedValue(str, 0, "");
     }
 }
