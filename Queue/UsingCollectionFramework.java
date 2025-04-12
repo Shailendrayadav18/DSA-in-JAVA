57d1856395691fb7fc4dc48d85891cecfef86182
Shailendrayadav18 <monuyadav1872002@gmail.com>
2025-04-12 15:33:29 +0530

    Implementation of Queue using Collection Framework

---
+++ Queue/UsingCollectionFramework.java
@@ -0,0 +1,21 @@
+package Queue;
+
+import java.util.*;
+
+public class UsingCollectionFramework {
+    public static void main(String[] args) {
+        //Queue<Integer> q=new LinkedList<>();
+        Queue<Integer> q=new ArrayDeque<>();
+
+        q.add(1);
+        q.add(2);
+        q.add(3); 
+        q.add(4);
+        q.remove();
+
+        while(!q.isEmpty()){
+            System.out.println(q.peek());
+            q.remove();
+        }
+    }
+}
